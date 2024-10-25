ad085ca910fdc33a9305747c62548251d00b2b1f
nicourrrn <s57111702@gmail.com>
2024-10-26 00:27:28 +0300

    Add consts

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.post("/encrypt")
+async def encrypt():
+    return {"message": "Encrypting message"}
+
+
+@app.post("/decrypt")
+async def decrypt():
+    return {"message": "Decrypting message"}
+
+
+async def is_key_weak(key: str) -> bool:
+    return False
+
+
+@app.post("/check_key")
+async def check_key(key: str):
+    return {"is_weak": await is_key_weak(key)}
